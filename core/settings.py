231b19d7bae4d0217a29f024de6ec552f4b47284
M R Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-12-30 14:48:24 +0600

    Integrate swagger for api documentation

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_swagger',       # Swagger 
+    'drf_yasg',                     # Swagger generator
     
     # local apps
     'sellers',
--- core/urls.py
+++ core/urls.py
@@ -17,10 +17,36 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from django.conf.urls.static import static
+from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf import settings
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Raktec",
+        default_version='v1',),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     # include urls from apps
     path('api/', include('sellers.urls')),
     path('api/', include('users.urls')),
+    
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
